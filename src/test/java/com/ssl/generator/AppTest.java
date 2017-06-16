2cc5d7ad15340148556943f1505606aa3d5368be
shisenlin <shisenlin@users.noreply.github.com>
2017-06-16 16:00:43 +0800

    mybatis 自动生成代码

---
+++ src/test/java/com/ssl/generator/AppTest.java
@@ -0,0 +1,12 @@
+package com.ssl.generator;
+
+/**
+ * Created by ssl on 2017/6/13.
+ */
+public class AppTest {
+    public static void main(String[] args) {
+/*        AppExample example = new AppExample();
+        example.setLimitStart(1);
+        example.setPageSize(10);*/
+    }
+}
